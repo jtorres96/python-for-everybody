058947ab8f552c071481be99f7b6b16dfb367e45
Juan Torres <35707228+jtorres96@users.noreply.github.com>
2021-01-24 18:51:01 -0500

    Added on 24/01/2021

---
+++ Chapter 2/Exercise 2.2.py
@@ -0,0 +1,10 @@
+
+
+# Exercise 2.2: Write a program that uses input to prompt a user for their name and then welcomes them.
+
+# For this we will use the input() function and string concatenation:
+
+name = input("Enter your name: ") # We set the function to ask for the user's name. Keep in mind that the variable will be a string, so if we input a number, it will be stored as a string, and therefore we can't perform any calculations with this...
+print ("Hello " + name) # We use the string stored in variable "name" and using string concatenation we print out a welcoming message.
+
+# Pretty simple right? However, this implementation raises a problem: What happens if we need to input data types different from strings? We will see this issue come up in future exercises.
